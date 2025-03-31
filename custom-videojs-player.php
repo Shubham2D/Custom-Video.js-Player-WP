7a2b64a223c82ba3a66246c82ed12bfa9422d70a
Shubham Sawarkar <shubhamsawarkar@live.com>
2025-03-31 02:48:12 -0400

    Add files via upload

---
+++ custom-videojs-player.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Plugin Name: Custom Video.js Player
+ * Plugin URI: https://github.com/your-username/Custom-Video.js-Player
+ * Description: A simple WordPress plugin to embed a Video.js player with a shortcode.
+ * Version: 1.0.0
+ * Author: Shubham Sawarkar
+ * Author URI: https://github.com/Shubham2D
+ * License: GPL-2.0-or-later
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) exit;
+
+// Enqueue Video.js assets
+function cvjp_enqueue_scripts() {
+    wp_enqueue_style('videojs-css', 'https://vjs.zencdn.net/8.5.2/video-js.css');
+    wp_enqueue_script('videojs-js', 'https://vjs.zencdn.net/8.5.2/video.min.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'cvjp_enqueue_scripts');
+
+// Shortcode to display Video.js player
+function cvjp_video_shortcode($atts) {
+    $atts = shortcode_atts([
+        'mp4' => '',
+        'webm' => '',
+        'width' => '720',
+        'height' => 'auto'
+    ], $atts, 'custom_videojs_player');
+    
+    if (empty($atts['mp4']) && empty($atts['webm'])) {
+        return '<p style="color: red;">Error: No video source provided.</p>';
+    }
+    
+    ob_start();
+    ?>
+    <div class="video-container" style="max-width: <?php echo esc_attr($atts['width']); ?>px; margin: auto;">
+        <video class="video-js vjs-default-skin" controls preload="auto" data-setup='{"fluid": true, "controlBar": { "pictureInPictureToggle": true }}'>
+            <?php if (!empty($atts['mp4'])): ?>
+                <source src="<?php echo esc_url($atts['mp4']); ?>" type="video/mp4">
+            <?php endif; ?>
+            <?php if (!empty($atts['webm'])): ?>
+                <source src="<?php echo esc_url($atts['webm']); ?>" type="video/webm">
+            <?php endif; ?>
+            Your browser does not support the video tag.
+        </video>
+    </div>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('custom_videojs_player', 'cvjp_video_shortcode');
